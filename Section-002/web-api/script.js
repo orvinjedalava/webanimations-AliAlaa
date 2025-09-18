45c94b3a70728977699fc09918b46036d6ccc6cb
orvinjedalava <ojedbub@gmail.com>
2025-09-18 10:22:34 +1000

    boink

--- Section-002/web-api/script.js
+++ Section-002/web-api/script.js
@@ -4,38 +4,33 @@ document.addEventListener("DOMContentLoaded", () => {
   // you can pause it by calling squareAnimation.pause()
   // you can play it again by calling squareAnimation.play()
   const squareAnimation = element.animate(
-    {
-      transform: ['translateX(0)', 'translateX(calc(100vw - 100px)) rotate(360deg)'], // offset [0,1]
-      backgroundColor: ['gold', 'blue', 'crimson'], // offset [0,0.5,1]
-      offset: [0, 0.3, 1],
-      easing: ['ease-in', 'linear'],
-      composite: ['add', 'replace', 'add']
-    },
-    // [
-    //   { 
-    //     transform: 'translateX(0)',
-    //     easing: 'ease-in', 
-    //   },
-    //   { 
-    //     backgroundColor: 'blue',
-    //     offset: 0.8,
-    //     composite: 'replace',
-    //   },
-    //   { 
-    //     transform: 'translateX(calc(100vw - 100px)) rotate(360deg)',
-    //     backgroundColor: 'crimson'
-    //   }
-    // ], 
+    
+    [
+      { 
+        transform: 'translateX(0)',
+        easing: 'ease-in', 
+      },
+      { 
+        backgroundColor: 'blue',
+        offset: 0.8,
+        composite: 'replace',
+      },
+      { 
+        transform: 'translateX(calc(100vw - 100px)) rotate(360deg)',
+        backgroundColor: 'crimson'
+      }
+    ], 
     {
       duration: 3000,
       delay: 1000,
-      direction: 'alternate',
+      direction: 'normal',
       fill: 'both',
       iterations: Infinity,
       easing: 'linear',
       composite: 'add',
+      iterationComposite: 'accumulate', // default is 'replace'. only works on firefox at this writing.
       timeline: document.timeline
     }
   )
-  
+  // squareAnimation.pause();
 });
